446b287cde8f61e5526c2b2cdb26374ad5c8f58a
abhilashrai28 <abhilashrai28@gmail.com>
2026-07-11 15:07:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
+provider "aws" {
+  region = var.region
+}
 
+module "vpc" {
+  source = "./module/VPC"
+
+  vpc_cidr = var.vpc_cidr
+  vpc_name = var.vpc_name
+}
+
+module "s3" {
+  source = "./module/S3"
+
+  bucket_name = var.bucket_name
+}
